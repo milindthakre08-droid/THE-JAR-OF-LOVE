bee225a0742ab18a74d58e5acee4c8f39ee93201
milindthakre08-droid <milindthakre08@gmail.com>
2026-06-14 16:14:14 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,3 +2,14 @@ function playMusic() {
   const music = document.getElementById("bgmusic");
   music.play();
 }
+
+document.addEventListener("DOMContentLoaded", () => {
+  const jar = document.getElementById("jar");
+
+  jar.addEventListener("click", () => {
+    const randomReason =
+      reasons[Math.floor(Math.random() * reasons.length)];
+
+    alert("💖 " + randomReason);
+  });
+});
