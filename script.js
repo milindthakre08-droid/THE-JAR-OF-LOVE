82d8cd0b6867dd336515b2d5f1e1da3b94f80688
milindthakre08-droid <milindthakre08@gmail.com>
2026-06-24 14:06:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-// ELEMENTS
-
 const music = document.getElementById("bgmusic");
 
 const enterBtn = document.getElementById("enterBtn");
@@ -10,27 +8,19 @@ const mainSite = document.getElementById("main-site");
 
 const revealBtn = document.getElementById("revealBtn");
 
-const card = document.getElementById("card");
-
 const reasonText = document.getElementById("reasonText");
 
-// ENTER WEBSITE
+const jar = document.getElementById("jar");
+
+const letter = document.getElementById("letter");
+
+/* ENTER */
 
 enterBtn.addEventListener("click", async () => {
 
     try {
-
-        music.volume = 0.6;
-
         await music.play();
-
-        console.log("Music started");
-
-    } catch (err) {
-
-        console.log("Music failed:", err);
-
-    }
+    } catch (e) {}
 
     welcomeScreen.style.opacity = "0";
 
@@ -44,36 +34,107 @@ enterBtn.addEventListener("click", async () => {
 
 });
 
-// MUSIC BUTTON
+/* MUSIC */
 
 function toggleMusic() {
 
     if (music.paused) {
 
-        music.play()
-            .then(() => {
-                console.log("Music resumed");
-            })
-            .catch(err => {
-                console.log(err);
-            });
+        music.play();
 
     } else {
 
         music.pause();
 
     }
+
 }
 
-// REVEAL REASON
+/* HEARTS */
+
+function createHeart() {
+
+    const heart = document.createElement("div");
+
+    heart.classList.add("heart");
+
+    heart.innerHTML = "💜";
+
+    const rect = jar.getBoundingClientRect();
+
+    heart.style.left = (rect.left + rect.width / 2) + "px";
+
+    heart.style.top = rect.top + "px";
+
+    document.body.appendChild(heart);
+
+    setTimeout(() => {
+
+        heart.remove();
+
+    }, 2500);
+
+}
+
+function burstHearts() {
+
+    for(let i=0;i<15;i++){
+
+        setTimeout(() => {
+
+            createHeart();
+
+        }, i*100);
+
+    }
+
+}
+
+/* TYPEWRITER */
+
+function typeWriter(text){
+
+    reasonText.innerHTML = "";
+
+    let i = 0;
+
+    const interval = setInterval(() => {
+
+        if(i < text.length){
+
+            reasonText.innerHTML += text.charAt(i);
+
+            i++;
+
+        } else {
+
+            clearInterval(interval);
+
+        }
+
+    }, 25);
+
+}
+
+/* MAIN */
 
 revealBtn.addEventListener("click", () => {
 
     const randomReason =
         reasons[Math.floor(Math.random() * reasons.length)];
 
-    reasonText.innerText = randomReason;
+    jar.classList.add("shake");
 
-    card.style.display = "block";
+    burstHearts();
+
+    setTimeout(() => {
+
+        jar.classList.remove("shake");
+
+        letter.classList.add("show");
+
+        typeWriter("Dear Lakku,\n\n" + randomReason + "\n\nLove,\nMilind 💜");
+
+    }, 700);
 
 });
